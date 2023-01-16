6a8d8bdabc008a9c329653ff973d8efe5bafebae
Jen Dunlap <jenniferleighdunlap@gmail.com>
2023-01-16 13:09:14 -0500

    user controllers

--- controllers/index.js
+++ controllers/index.js
@@ -2,6 +2,8 @@ const Sign = require('../models/sign')
 const User = require('../models/user')
 const Post = require('../models/post')
 
+// SIGNS
+
 const createSign = async (req, res) => {
   try {
     const sign = await new Sign(req.body)
@@ -48,9 +50,75 @@ const updateSign = async (req, res) => {
   }
 }
 
+// USERS
+
+const createUser = async (req, res) => {
+  try {
+    const user = await new User(req.body)
+    await user.save()
+    return res.status(201).json({
+      user
+    })
+  } catch (error) {
+    return res.status(500).json({ error: error.message })
+  }
+}
+
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find()
+    return res.status(200).json({ users })
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
+const getUserById = async (req, res) => {
+  try {
+    const { id } = req.params
+    const user = await User.findById(id)
+    if (user) {
+      return res.status(200).json({ user })
+    }
+    return res.status(404).send('The User does not exist')
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
+const updateUser = async (req, res) => {
+  console.log(req.body)
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true
+    })
+    res.status(200).json(user)
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
+const deleteUser = async (req, res) => {
+  try {
+    const { id } = req.params
+    const deleted = await User.findByIdAndDelete(id)
+    if (deleted) {
+      return res.status(200).send('User deleted')
+    }
+    throw new Error('User not found')
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
 module.exports = {
   createSign,
   getAllSigns,
   getSignById,
-  updateSign
+  updateSign,
+  createUser,
+  getAllUsers,
+  getUserById,
+  updateUser,
+  deleteUser
 }
