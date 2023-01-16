9425cdeb354fdf94fdf4cac044eb5a683955fa4c
Jen Dunlap <jenniferleighdunlap@gmail.com>
2023-01-16 13:18:46 -0500

    post controllers

--- controllers/index.js
+++ controllers/index.js
@@ -111,6 +111,67 @@ const deleteUser = async (req, res) => {
   }
 }
 
+// POSTS
+
+const createPost = async (req, res) => {
+  try {
+    const post = await new Post(req.body)
+    await post.save()
+    return res.status(201).json({
+      post
+    })
+  } catch (error) {
+    return res.status(500).json({ error: error.message })
+  }
+}
+
+const getAllPosts = async (req, res) => {
+  try {
+    const post = await Post.find()
+    return res.status(200).json({ posts })
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
+const getPostById = async (req, res) => {
+  try {
+    const { id } = req.params
+    const post = await Post.findById(id)
+    if (post) {
+      return res.status(200).json({ post })
+    }
+    return res.status(404).send('The Post does not exist')
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
+const updatePost = async (req, res) => {
+  console.log(req.body)
+  try {
+    const post = await Post.findByIdAndUpdate(req.params.id, req.body, {
+      new: true
+    })
+    res.status(200).json(post)
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
+const deletePost = async (req, res) => {
+  try {
+    const { id } = req.params
+    const deleted = await Post.findByIdAndDelete(id)
+    if (deleted) {
+      return res.status(200).send('Post deleted')
+    }
+    throw new Error('Post not found')
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
 module.exports = {
   createSign,
   getAllSigns,
@@ -120,5 +181,10 @@ module.exports = {
   getAllUsers,
   getUserById,
   updateUser,
-  deleteUser
+  deleteUser,
+  createPost,
+  getAllPosts,
+  getPostById,
+  updatePost,
+  deletePost
 }
