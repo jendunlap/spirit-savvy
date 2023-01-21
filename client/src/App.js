3213e75ce3d715dec1285dfea6b7d21ff413f4bb
Jen Dunlap <jenniferleighdunlap@gmail.com>
2023-01-21 17:40:13 -0500

    added routes for new pages to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,12 @@ import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Nav from './components/Nav'
 import Home from './pages/Home'
+import About from './pages/About'
+import Reviews from './pages/Reviews'
+import Services from './pages/Services'
+import Signs from './pages/Signs'
+import Posts from './pages/Posts'
+import Cards from './pages/Cards'
 
 const App = () => {
   return (
@@ -10,6 +16,12 @@ const App = () => {
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/reviews" element={<Reviews />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/signs" element={<Signs />} />
+          <Route path="/posts" element={<Posts />} />
+          <Route path="/cards" element={<Cards />} />
         </Routes>
       </main>
     </div>
