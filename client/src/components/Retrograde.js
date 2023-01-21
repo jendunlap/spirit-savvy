4d9a57e1a64f45cc015c3cd855ddb2425ca3cb60
Jen Dunlap <jenniferleighdunlap@gmail.com>
2023-01-21 17:13:12 -0500

    pages and components created

---
+++ client/src/components/Retrograde.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Retrograde = () => {
+  return <div>Retrograde</div>
+}
+
+export default Retrograde
---
+++ client/src/pages/Signs.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Signs = () => {
+  return <div>Signs</div>
+}
+
+export default Signs
