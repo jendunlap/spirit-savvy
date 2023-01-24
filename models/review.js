71863b7f6830713caaa7a2f82df513cf1de94054
Jen Dunlap <jenniferleighdunlap@gmail.com>
2023-01-23 22:39:06 -0500

    changes to review model

--- models/review.js
+++ models/review.js
@@ -4,6 +4,8 @@ const Schema = mongoose.Schema
 const Review = new Schema(
   {
     title: { type: String, required: false },
+    author: { type: String, required: false },
+    snippet: { type: String, required: false },
     text: { type: String, required: true }
   },
   { timestamps: true }
